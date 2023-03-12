bd7e3232022f9d242efba6b52dc6cd6c6570a069
Raphael Moraes <raphaelfm.2012@gmail.com>
2023-03-12 14:24:08 -0300

    First Commit

---
+++ AnliseHospitais/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AnliseHospitais.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AnliseHospitais/Models/HospitalModel.cs
@@ -0,0 +1,19 @@
+﻿namespace AnliseHospitais.Models
+{
+    public class HospitalModel
+    {
+        public int Comp { get; set; }
+        public string? Regiao { get; set; }
+        public string? Uf { get; set; }
+        public string? Muicipio { get; set;}
+        public string? MotivoDesabilitacao { get; set; }
+        public string? CNES { get; set; }
+        public string? NomeEstabelecimento { get; set; }
+        public string? RazaoSocial { get; set; }
+        public string? TpGestao { get; set; }
+        public string? CodigoTipoUnidade { get; set;}
+        public string? DescricaoTipoUnidade { get; set; }
+
+    }
+}
+//COMP,"REGIAO","UF","MUNICIPIO","MOTIVO DESABILITACAO","CNES","NOME ESTABELECIMENTO","RAZAO SOCIAL","TP_GESTAO","CO_TIPO_UNIDADE","DS_TIPO_UNIDADE","NATUREZA_JURIDICA","DESC_NATUREZA_JURIDICA","NO_LOGRADOURO","NU_ENDERECO","NO_COMPLEMENTO","NO_BAIRRO","CO_CEP","NU_TELEFONE","NO_EMAIL","LEITOS EXISTENTE","LEITOS SUS","UTI TOTAL - EXIST","UTI TOTAL - SUS","UTI ADULTO - EXIST","UTI ADULTO - SUS","UTI PEDIATRICO - EXIST","UTI PEDIATRICO - SUS","UTI NEONATAL - EXIST","UTI NEONATAL - SUS","UTI QUEIMADO - EXIST","UTI QUEIMADO - SUS","UTI CORONARIANA - EXIST","UTI CORONARIANA - SUS"
