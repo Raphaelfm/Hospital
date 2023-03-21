33655574a0845f849fe944b9d2c51f77f2fe8886
Raphael Moraes <raphaelfm.2012@gmail.com>
2023-03-21 15:46:47 -0300

    Ajusta utilitário e controller para ler e gravar registros do excel para o banco de dados.
    Adiciona nova model para otimizar processo de leitura.

---
+++ AnliseHospitais/Data/Migrations/20230321184118_Ajuste.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnliseHospitais.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Ajuste : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Muicipio",
+                table: "Hospitais",
+                newName: "Municipio");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Municipio",
+                table: "Hospitais",
+                newName: "Muicipio");
+        }
+    }
+}
--- AnliseHospitais/Utils/Utilitarios.cs
+++ AnliseHospitais/Utils/Utilitarios.cs
@@ -13,8 +13,47 @@ namespace AnliseHospitais.Utils
             using (var reader = new StreamReader(caminhoArquivo))
             using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = "," }))
             {
-                var records = csv.GetRecords<HospitalModel>().ToList();
-                return records;
+                var records = csv.GetRecords<HospitaisCsvModel>().ToList();
+
+                var hospitalModel = records.Select((record, index) => new HospitalModel
+                {
+                    Comp = record.Comp,
+                    Regiao = record.Regiao,
+                    Uf = record.Uf,
+                    Municipio = record.Municipio,
+                    MotivoDesabilitacao = record.MotivoDesabilitacao,
+                    CNES = record.CNES,
+                    NomeEstabelecimento = record.CNES,
+                    RazaoSocial = record.RazaoSocial,
+                    TpGestao = record.TpGestao,
+                    CodigoTipoUnidade = record.CodigoTipoUnidade,
+                    DescricaoTipoUnidade = record.DescricaoTipoUnidade,
+                    NaturezaJuridica = record.NaturezaJuridica,
+                    DescricaoNatureza = record.DescricaoNatureza,
+                    NomeLogradouro = record.NomeLogradouro,
+                    NumeroEndereco = record.NumeroEndereco,
+                    NomeComplemento = record.NomeComplemento,
+                    NomeBairro = record.NomeBairro,
+                    CodigoCep = record.CodigoCep,
+                    NumeroTelefone = record.NumeroTelefone,
+                    Email = record.Email,
+                    LeitosExistentes = record.LeitosExistentes,
+                    LeitosSus = record.LeitosSus,
+                    UtiTotalExistentes = record.UtiTotalExistentes,
+                    UtiTotalSus = record.UtiTotalSus,
+                    UtiAdultoExistente = record.UtiAdultoExistente,
+                    UtiAdultoSus = record.UtiAdultoSus,
+                    UtiPediatricoExistentes = record.UtiPediatricoExistentes,
+                    UtiPediatricoSus = record.UtiPediatricoSus,
+                    UtiNeoNatalExistentes = record.UtiNeoNatalExistentes,
+                    UtiNeoNatalSus = record.UtiNeoNatalSus,
+                    UtiQueimadoExistentes = record.UtiQueimadoExistentes,
+                    UtiQueimadoSus = record.UtiQueimadoSus,
+                    UtiCoronarianaExistentes = record.UtiCoronarianaExistentes,
+                    UtiCoronarianaSus = record.UtiCoronarianaSus
+                }).ToList();
+
+                return hospitalModel;
             }
         }
 
