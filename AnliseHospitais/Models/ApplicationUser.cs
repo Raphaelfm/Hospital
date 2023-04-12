2d7e8ca72d9d90ba4ace4da34f6b50081ab18b7f
Raphael Moraes <raphaelfm.2012@gmail.com>
2023-04-12 17:50:53 -0300

    Ajusta view de login, e adiciona campo para nome no momento do registro de usuários

---
+++ AnliseHospitais/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AnliseHospitais.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? UserCompleteName { get; set; }
+    }
+}
---
+++ AnliseHospitais/Models/ChangeUserCompleteNameModel.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AnliseHospitais.Models
+{
+    public class ChangeUserCompleteNameModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public ChangeUserCompleteNameModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [BindProperty]
+        public string UserCompleteName { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string ReturnUrl { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            user.UserCompleteName = UserCompleteName;
+            var result = await _userManager.UpdateAsync(user);
+            if (!result.Succeeded)
+            {
+                throw new InvalidOperationException($"Unexpected error occurred updating user with ID '{user.Id}'.");
+            }
+
+            await _signInManager.RefreshSignInAsync(user);
+            TempData["StatusMessage"] = "Your profile has been updated";
+            return RedirectToPage("./Index");
+        }
+    }
+
+}
