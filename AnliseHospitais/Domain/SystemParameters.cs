0c165c7e1f0b25d346b76736e31456bd432b3290
Raphael Moraes <raphaelfm.2012@gmail.com>
2023-05-25 17:58:02 -0300

    Ajusta view de importação de dados

---
+++ AnliseHospitais/Domain/SystemParameters.cs
@@ -0,0 +1,10 @@
+﻿namespace AnliseHospitais.Domain
+{
+    public class SystemParameters
+    {
+        /// <summary>
+        /// List of allowed languages/cultures
+        /// </summary>
+        public static List<string> Languages {  get; set; }
+    }
+}
