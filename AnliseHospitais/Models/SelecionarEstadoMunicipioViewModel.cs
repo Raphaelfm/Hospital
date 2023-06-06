678280c57dc4a15b70225690941416ee50484e8a
Raphael Moraes <raphaelfm.2012@gmail.com>
2023-06-06 17:37:35 -0300

    Adiciona Comparacao

---
+++ AnliseHospitais/Models/SelecionarEstadoMunicipioViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AnliseHospitais.Models
+{
+    public class SelecionarEstadoMunicipioViewModel
+    {
+        public List<string> Estados { get; set; }
+        public List<string> Municipios { get; set; }
+        public string EstadoSelecionado { get; set; }
+        public string MunicipioSelecionado { get; set; }
+    }
+}
